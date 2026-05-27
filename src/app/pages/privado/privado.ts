2fd69566ed0a4fd918daebe521d3ec60d1148c58
epina <eduardo.gomez@people-media.com.mx>
2026-05-27 08:53:57 -0600

    navbar y apartados de navegación internos

---
+++ src/app/pages/privado/privado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privado',
+  imports: [],
+  templateUrl: './privado.html',
+  styleUrl: './privado.scss',
+})
+export class Privado {}
--- src/app/pages/publico/pages/inicio-sesion/inicio-sesion.ts
+++ src/app/pages/publico/pages/inicio-sesion/inicio-sesion.ts
@@ -3,11 +3,10 @@ import { Card } from 'primeng/card';
 import { Button } from 'primeng/button';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputText } from 'primeng/inputtext';
-import { StyleClass } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [Card, Button, ReactiveFormsModule, InputText, StyleClass],
+  imports: [Card, Button, ReactiveFormsModule, InputText],
   templateUrl: './inicio-sesion.html',
   styleUrl: './inicio-sesion.scss',
   host: {
