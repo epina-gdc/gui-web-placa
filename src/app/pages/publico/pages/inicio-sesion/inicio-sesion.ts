e70dbe1ed3eab5c5979ac3936eae03bf569f886b
epina <eduardo.gomez@people-media.com.mx>
2026-05-26 18:59:04 -0600

    esquema inicial -> login

--- src/app/pages/publico/pages/inicio-sesion/inicio-sesion.ts
+++ src/app/pages/publico/pages/inicio-sesion/inicio-sesion.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputText } from 'primeng/inputtext';
+import { StyleClass } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [Card],
+  imports: [Card, Button, ReactiveFormsModule, InputText, StyleClass],
   templateUrl: './inicio-sesion.html',
   styleUrl: './inicio-sesion.scss',
 })
-export class InicioSesion {}
+export class InicioSesion {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.inicializarForm();
+  }
+
+  loginForm!: FormGroup;
+
+  inicializarForm(): FormGroup {
+    return this.formBuilder.group({
+      matricula: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  iniciarSesion(): void {}
+}
