ed7404bd5d138af4250dd3b2c53cd9e14dcf3bec
epina <eduardo.gomez@people-media.com.mx>
2026-05-27 20:40:12 -0600

    creación de componentes faltantes de sección privado

---
+++ src/app/pages/privado/pages/solicitud-extraordinaria-proveedor/solicitud-extraordinaria-proveedor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud-extraordinaria-proveedor',
+  imports: [],
+  templateUrl: './solicitud-extraordinaria-proveedor.html',
+  styleUrl: './solicitud-extraordinaria-proveedor.scss',
+})
+export class SolicitudExtraordinariaProveedor {}
