2adcaec3bf106c8ae10c799e3c2e4de307d345ab
epina <eduardo.gomez@people-media.com.mx>
2026-05-29 15:31:12 -0600

    css en seccion-busqueda

--- src/app/components/seccion-busqueda/seccion-busqueda.ts
+++ src/app/components/seccion-busqueda/seccion-busqueda.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-seccion-busqueda',
@@ -15,6 +15,7 @@ import { Component, input } from '@angular/core';
     </div>
   `,
   styleUrl: './seccion-busqueda.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class SeccionBusqueda {
   titulo = input.required<string>();
