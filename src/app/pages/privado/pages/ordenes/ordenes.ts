2a2b677d8c50fae5e7c50a35b7304ddad067ccbc
josesaldanapeople <jose.saldana@people-media.com.mx>
2026-06-01 11:28:18 -0600

    pantalla de ordenes

--- src/app/pages/privado/pages/ordenes/ordenes.ts
+++ src/app/pages/privado/pages/ordenes/ordenes.ts
@@ -1,9 +1,107 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Anio } from '@core/models/anio.interface';
+import { Ooad } from '@core/models/ooad.interface';
+import { Periodo } from '@core/models/periodo.interface';
+import { Servicio } from '@core/models/servicio.interface';
+import { TipoReporte } from '@core/models/tipo-reporte.interface';
+import { Unidad } from '@core/models/unidad.interface';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { TipoOrden } from '@core/models/orden.interface';
 
 @Component({
   selector: 'app-ordenes',
-  imports: [],
+  imports: [ReactiveFormsModule, SelectModule, ButtonModule, CommonModule,DatePickerModule, RadioButtonModule],
   templateUrl: './ordenes.html',
   styleUrl: './ordenes.scss',
 })
-export class Ordenes {}
+export class Ordenes {
+filtroForm!: FormGroup;
+fb: FormBuilder = inject(FormBuilder);
+  
+tiposOrdenes: TipoOrden[] = [
+    {
+      idTipoOrden: 1,
+      desTipoOrden: "Órden de compra"
+    },
+    {
+      idTipoOrden: 2,
+      desTipoOrden: "Requisición interna"
+    }
+  ];
+
+  ooads: Ooad[] = [
+    {
+      idOoad: 1,
+      desOoad: "OOAD 1"
+    },
+    {
+      idOoad: 2,
+      desOoad: "OOAD 2"
+    }
+  ];
+
+  unidades: Unidad[] = [
+    {
+      idUnidad: 1,
+      desUnidad: "Unidad 1"
+    },
+    {
+      idUnidad: 2,
+      desUnidad: "Unidad 2"
+    }
+  ];
+
+  servicios:Servicio[] = [
+    {
+      idServicio: 1,
+      desServicio: "Servicio 1"
+    },
+    {
+      idServicio: 2,
+      desServicio: "Servicio 2"
+    }
+  ];
+  years:Anio[] = [
+    {
+      idAnio: 2022,
+      desAnio: "2022"
+    },
+    {
+      idAnio: 2023,
+      desAnio: "2023"
+    }
+  ];
+  periodos:Periodo[] = [
+    {
+      idPeriodo: 1,
+      desPeriodo: "Periodo 1"
+    },
+    {
+      idPeriodo: 2,
+      desPeriodo: "Periodo 2"
+    }
+  ];
+  constructor() {
+    this.filtroForm = this.fb.group({
+      tipoOrden: [null],
+      ooad: [null],
+      unidad: [null],
+      servicio: [null],
+      anio: [null],
+      periodo: [null],
+      fecha: [null],
+      formato: [null]
+    });
+   }
+   generarReporte(): void {
+
+  console.log('Generar reporte');
+
+}
+
+}
