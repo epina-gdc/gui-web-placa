77f5e4df34ae75995005f3b3129b7c55e6f26be1
epina <eduardo.gomez@people-media.com.mx>
2026-05-31 23:06:44 -0600

    minuta sintetica previa

--- src/app/pages/privado/pages/minuta-sintetica/minuta-sintetica.ts
+++ src/app/pages/privado/pages/minuta-sintetica/minuta-sintetica.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SeccionTitulos } from '@components/seccion-titulos/seccion-titulos';
+import { SeccionBusqueda } from '@components/seccion-busqueda/seccion-busqueda';
 
 @Component({
   selector: 'app-minuta-sintetica',
-  imports: [],
+  imports: [SeccionTitulos, SeccionBusqueda],
   templateUrl: './minuta-sintetica.html',
   styleUrl: './minuta-sintetica.scss',
 })
